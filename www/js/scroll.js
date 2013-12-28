677c4c40fd55858c31ba24e1927daae95cfad6cb
cdimoff <chrisdimoff@live.com>
2013-12-28 14:43:16 -0800

    updates

---
+++ www/js/scroll.js
@@ -0,0 +1,6 @@
+$(window).scroll(function(e){ 
+  $el = $('.fixedElement'); 
+  if ($(this).scrollTop() > 200 && $el.css('position') != 'fixed'){ 
+    $('.fixedElement').css({'position': 'fixed', 'top': '0px'}); 
+  } 
+});
